130bf832eca2736bddf7e4ad07ac234b040ced67
adminlyx <2358072658@qq.com>
2025-04-30 17:10:22 +0800

    feat(sky-server): 添加员工分页查询功能并优化异常处理- 在 EmployeeController 中添加分页查询接口 - 在 EmployeeServiceImpl 中实现分页查询逻辑 - 在 GlobalExceptionHandler 中添加 SQL 异常处理 - 更新 JwtTokenAdminInterceptor 以设置当前用户 ID - 修改 .gitignore 文件，忽略更多临时文件

--- sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -77,9 +78,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
 
-        // todo 改为当前登录用户ID
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         // insert mapper
         employeeMapper.insert(employee);
