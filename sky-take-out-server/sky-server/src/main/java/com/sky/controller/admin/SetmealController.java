9deaa2f98b1506d1d817f85cf943540cac70a72f
adminlyx <2358072658@qq.com>
2025-08-03 10:59:28 +0800

    feat(cache): 实现套餐数据缓存功能
    - 在 SetmealController 中添加 CacheEvict 注解，用于在增删改操作时清除缓存
    - 在 UserSetmealController 中添加 Cacheable 注解，用于缓存套餐列表查询结果- 在 SkyApplication 中添加 EnableCaching 注解，开启缓存功能

--- sky-take-out-server/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-take-out-server/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +35,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO) {
         log.info("新增套餐：{}", setmealDTO);
         setmealService.saveWithDish(setmealDTO);
@@ -60,6 +62,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @ApiOperation("批量删除")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result delete(@RequestParam List<Long> ids) {
         log.info("批量删除：{}", ids);
         setmealService.delete(ids);
@@ -86,6 +89,7 @@ public class SetmealController {
      */
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO) {
         log.info("修改套餐：{}", setmealDTO);
         setmealService.update(setmealDTO);
@@ -100,6 +104,7 @@ public class SetmealController {
      */
     @PostMapping("status/{status}")
     @ApiOperation("套餐起售停售")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id) {
         log.info("套餐起售停售：{}, {}", id, status);
         setmealService.startOrStop(id, status);
