3aacdcc494dc10d4c385e3edade32102f91fd1a1
adminlyx <2358072658@qq.com>
2025-08-24 18:33:16 +0800

    feat(order): 添加客户催单功能并优化订单相关接口
    - 在 OrderController 中添加了客户催单的接口
    - 在 OrderService接口中添加了客户催单的方法
    - 在 OrderServiceImpl 中实现了客户催单的功能
    - 在 OrderMapper.xml 中为订单查询结果添加了按下单时间降序排序

--- sky-take-out-server/sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-take-out-server/sky-server/src/main/java/com/sky/service/OrderService.java
@@ -98,4 +98,11 @@ public interface OrderService {
      * @return
      */
     void complete(Long id);
+
+    /**
+     * 客户催单
+     * @param id
+     * @return
+     */
+    void reminder(Long id);
 }
--- sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -463,8 +463,8 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public void complete(Long id) {
-        Orders ordersDB = orderMapper.getById(id);
 
+        Orders ordersDB = orderMapper.getById(id);
         if(ordersDB == null || !ordersDB.getStatus().equals(Orders.DELIVERY_IN_PROGRESS)) {
             throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
         }
@@ -476,5 +476,28 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+    }
+
+
+    /**
+     * 客户催单
+     * @param id
+     * @return
+     */
+    @Override
+    public void reminder(Long id) {
+
+        Orders ordersDB = orderMapper.getById(id);
+        if(ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("type", 2);
+        map.put("orderId", id);
+        map.put("content", "订单号：" + ordersDB.getNumber());
+
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
     }
 }
