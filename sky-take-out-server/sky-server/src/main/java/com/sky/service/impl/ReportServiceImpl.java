0ba1ee5744c12ada55b1acefa0fff95535452cc1
adminlyx <2358072658@qq.com>
2025-08-30 11:37:22 +0800

    feat(sky-server): 导出运营数据报表功能
    - 新增 ReportController 中的 export 方法，实现导出运营数据报表的功能
    - 在 ReportService 接口中添加 exportBusinessData 方法，用于导出运营数据
    - 实现 ReportServiceImpl 中的 exportBusinessData 方法，包括获取业务数据、写入 Excel 文件和下载报表
    - 更新 access.log 中的访问记录，增加导出报表相关的请求

--- sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-take-out-server/sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -5,15 +5,19 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
-import com.sky.vo.OrderReportVO;
-import com.sky.vo.SalesTop10ReportVO;
-import com.sky.vo.TurnoverReportVO;
-import com.sky.vo.UserReportVO;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -32,6 +36,8 @@ public class ReportServiceImpl implements ReportService {
     private OrderMapper orderMapper;
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private WorkspaceService workspaceService;
 
 
     /**
@@ -182,5 +188,66 @@ public class ReportServiceImpl implements ReportService {
                 .nameList(StringUtils.join(nameList, ","))
                 .numberList(StringUtils.join(numberList, ","))
                 .build();
+    }
+
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @Override
+    public void exportBusinessData(HttpServletResponse response) {
+
+        // 1. get business data
+        LocalDate beginDate = LocalDate.now().minusDays(30);
+        LocalDate endDate = LocalDate.now().minusDays(1);
+        BusinessDataVO businessData = workspaceService.getBusinessData(
+                LocalDateTime.of(beginDate, LocalTime.MIN),
+                LocalDateTime.of(endDate, LocalTime.MAX)
+        );
+
+        // 2. write excel
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("template/运营数据报表模板.xlsx");
+        try {
+            if (inputStream != null) {
+                XSSFWorkbook excel = new XSSFWorkbook(inputStream);
+                XSSFSheet sheet1 = excel.getSheet("sheet1");
+
+                // overview data
+                sheet1.getRow(1).getCell(1).setCellValue("时间：" + beginDate + "至" + endDate);
+                sheet1.getRow(3).getCell(2).setCellValue(businessData.getTurnover());
+                sheet1.getRow(3).getCell(4).setCellValue(businessData.getOrderCompletionRate());
+                sheet1.getRow(3).getCell(6).setCellValue(businessData.getNewUsers());
+                sheet1.getRow(4).getCell(2).setCellValue(businessData.getValidOrderCount());
+                sheet1.getRow(4).getCell(4).setCellValue(businessData.getUnitPrice());
+
+                // detail data
+                for (int i = 0; i < 30; i++) {
+                    LocalDate date = beginDate.plusDays(i);
+                    BusinessDataVO data = workspaceService.getBusinessData(
+                            LocalDateTime.of(date, LocalTime.MIN),
+                            LocalDateTime.of(date, LocalTime.MAX)
+                    );
+
+                    sheet1.getRow(i+7).getCell(1).setCellValue(date.toString());
+                    sheet1.getRow(i+7).getCell(2).setCellValue(data.getTurnover());
+                    sheet1.getRow(i+7).getCell(3).setCellValue(data.getValidOrderCount());
+                    sheet1.getRow(i+7).getCell(4).setCellValue(data.getOrderCompletionRate());
+                    sheet1.getRow(i+7).getCell(5).setCellValue(data.getUnitPrice());
+                    sheet1.getRow(i+7).getCell(6).setCellValue(data.getNewUsers());
+
+                }
+
+                // 3. download excel
+                ServletOutputStream outputStream = response.getOutputStream();
+                excel.write(outputStream);
+
+                outputStream.close();
+                excel.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
